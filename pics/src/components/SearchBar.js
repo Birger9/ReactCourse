c8b6ea95c26063d02549de53ead4e248fa24b834
Erik <eribi813@student.liu.se>
2022-03-17 10:19:08 +0100

    "Working on picture project"

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <Form>
+                    <input type="text" />
+                </Form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
