23f338971842f79525b0b1a519d24342d9298365
Erik <erikbirgersson98@gmail.com>
2022-03-17 15:05:25 +0100

    "Working on pics project"

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    state = {term: ""};
+
     render() {
         return (
-            <div>
-                <Form>
-                    <input type="text" />
-                </Form>
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image search</label>
+                        <input type="text" value={this.state.term} onChange={(e) => this.setState({ term:  e.target.value})}/>
+                    </div>
+                </form>
             </div>
         );
     }
