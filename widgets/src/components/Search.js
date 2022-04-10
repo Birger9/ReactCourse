2239c528c89cde9feba42f90658ed2c5f567afef
Erik <eribi813@student.liu.se>
2022-04-10 13:32:30 +0200

    "Minor changes made"

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -25,6 +25,19 @@ const Search = () => {
         }
     }, [term]);
 
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    {result.snippet}
+                </div>
+            </div>
+        );
+    });
+
     return (
         <div>
             <div className="ui form">
@@ -37,6 +50,7 @@ const Search = () => {
                     />
                 </div>
             </div>
+            <div className="ui celled list">{renderedResults}</div>
         </div>
     );
 };
